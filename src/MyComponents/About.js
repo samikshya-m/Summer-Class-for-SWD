4b57012e8e724bf4940347bec36e2e4fc9709929
samikshya maharjan <samikshyamaharjan9@gmail.com>
2026-07-14 12:19:45 +0545

    My First React Project List

---
+++ src/MyComponents/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About=()=>{
+    return(
+        <div className='container my-5'>
+            <h2>
+                About my Todo List
+            </h2>
+            <p>
+                This is a simple todo list project where i can add todo and delete todo.
+            </p>
+            <p>
+                Feature available only-Add, view and delete.
+            </p>
+
+        </div>
+    );
+};
+
+export {About};
+// export default About;
